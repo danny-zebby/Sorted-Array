3051c66c835858409c6e535f0cb9d4bd9d969474
Danny <drodriguez125@horizon.csueastbay.edu>
2023-02-26 15:13:56 -0800

    done

--- SortedList.cpp
+++ SortedList.cpp
@@ -25,18 +25,47 @@ int SortedList<T>::GetLength() const {
 
 template<class T>
 bool SortedList<T>::Contains(T someItem) {
-
+    int mid, first =0, last= length-1;
+    while(first<=last){
+        mid=(first+last)/2;
+        if(someItem<info[mid])
+            last=mid-1;
+        else if(someItem>info[mid])
+            first = mid+1;
+        else
+            return true;
+    }
     return false;
 }
 
 template<class T>
 void SortedList<T>::PutItem(T item) {
-
+    if(IsFull())
+        return;
+    length++;
+    for(int i =0; i< length; i++){
+        if(info[i] > item){
+            for(int j = length-1; j>=i;j--)
+                info[j+1]=info[j];
+            info[i] = item;
+            return;
+        }
+    }info[length] = item;
 }
 
 template<class T>
 void SortedList<T>::DeleteItem(T item) {
-
+    for(int i=0; i<length;i++){
+        if(info[i] == item){
+            if(i == length -1){
+                length--;
+                return;
+            }for(int j = i+1; j< length;j++)
+                info[j-i]=info[j];
+            --length;
+            return;
+        }
+    }
 }
 
 template<class T>
